7966c59afc04b9c6bd17e5dd75017bf29f31f5ab
Neven Bedr <neven2225@gmail.com>
2020-05-20 12:28:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("This is really cool!");
+
+    document.body.innerHTML = document.body.innerHTML.replace('JavaScript is so cool. It lets me add text to my page programmatically.', 'This is really cool!');
+});
